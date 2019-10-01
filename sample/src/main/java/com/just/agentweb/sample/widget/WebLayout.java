5777f51b2639318e5e7759e8c7542c87c617b756
Json2String2 <53180077+Json2String2@users.noreply.github.com>
2019-10-01 16:00:26 +0800

    Update WebLayout.java

--- sample/src/main/java/com/just/agentweb/sample/widget/WebLayout.java
+++ sample/src/main/java/com/just/agentweb/sample/widget/WebLayout.java
@@ -1,7 +1,6 @@
 package com.just.agentweb.sample.widget;
 
 import android.app.Activity;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
